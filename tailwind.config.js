d49d1b7abd6d3f6e04011765baed2d36865dea5e
Diego Chaparro <contacto@institutolael.cl>
2026-02-08 21:38:20 -0300

    Primer deploy sitio Naama Studio

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'playfair': ['"Playfair Display"', 'serif'],
+        'dancing': ['"Dancing Script"', 'cursive'],
+        'montserrat': ['"Montserrat"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
